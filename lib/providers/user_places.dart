33ca87f5db781395e23e7a20c47c78a3d7205e8b
Samir Qatim <sqatim@student.1337.ma>
2024-02-12 17:02:05 +0100

    start project

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title, File image) {
+    state = [Place(title: title, image: image), ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>((ref) {
+  return UserPlacesNotifier();
+});
